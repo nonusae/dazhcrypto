9c563cb36b7a3f74147107636876824d99b22338
nonusae <nonusae@gmail.com>
2020-12-04 18:08:51 +0700

    Add AppBar and grid

--- src/App/index.js
+++ src/App/index.js
@@ -2,10 +2,12 @@ import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 import styled from 'styled-components'
 import AppLayout from './AppLayout'
+import AppBar from './AppBar'
 
 function App() {
   return (
     <AppLayout>
+      <AppBar></AppBar>
       <WelcomeMessage />
     </AppLayout>
   );
