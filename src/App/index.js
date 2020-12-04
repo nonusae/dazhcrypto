3a2e5fd37090ef05f4a87c065dfe62adc8dfb118
nonusae <nonusae@gmail.com>
2020-12-04 18:01:50 +0700

    Add AppLayout

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,13 @@
 import './App.css';
 import WelcomeMessage from './WelcomeMessage'
 import styled from 'styled-components'
+import AppLayout from './AppLayout'
 
 function App() {
   return (
-    <div>
+    <AppLayout>
       <WelcomeMessage />
-    </div>
+    </AppLayout>
   );
 }
 
