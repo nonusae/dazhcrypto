b93e946e9de9eae2533fd2681b63c682d9963cee
nonusae <nonusae@gmail.com>
2020-12-05 15:28:03 +0700

    Add CoinGrid

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import { AppContext } from '../App/AppProvider';
+
+
+export const CoinGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr)
+`
+
+export default function coinGrid() {
+  return <AppContext.Consumer>
+    { ({coinList}) =>
+      <CoinGridStyled>
+        {Object.keys(coinList).map(coinKey =>
+          <div> {coinKey} </div>
+        )}
+      </CoinGridStyled>
+    }
+  </AppContext.Consumer>
+}
