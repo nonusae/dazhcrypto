f4d11606f12ce7931587c2d987cdeddad18d7612
nonusae <nonusae@gmail.com>
2020-12-05 16:15:44 +0700

    Create CoinGrid and Tile

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
-
+import { SelectableTile } from '../Shared/Tile';
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr)
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 15px;
 `
 
 export default function coinGrid() {
@@ -13,7 +14,7 @@ export default function coinGrid() {
     { ({coinList}) =>
       <CoinGridStyled>
         {Object.keys(coinList).map(coinKey =>
-          <div> {coinKey} </div>
+          <SelectableTile> {coinKey} </SelectableTile>
         )}
       </CoinGridStyled>
     }
