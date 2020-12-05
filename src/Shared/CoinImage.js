2c9a1327a4a8ba90ec51eb297da0620218613210
nonusae <nonusae@gmail.com>
2020-12-05 16:53:16 +0700

    Add CoinTile

---
+++ src/Shared/CoinImage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CoinImage({coin, style}) {
+  return <img
+    alt={coin.CoinSymbol}
+    style={style || {height: '50px'}}
+    src={`http://cryptocompare.com/${
+      coin.ImageUrl
+    }`}
+  />
+};
+
