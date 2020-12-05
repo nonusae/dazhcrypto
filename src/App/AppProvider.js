0d64979e00f75acc4c84286955dbe08b25bdfa42
nonusae <nonusae@gmail.com>
2020-12-05 12:40:00 +0700

    Add local Storage for handle comfirm favorite

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -6,11 +6,32 @@ export class AppProvider extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'settings',
+      page: 'dashboard',
+      ...this.savedSettings(),
       setPage: this.setPage,
+      confirmFavorites: this.confirmFavorites
     }
   }
+
+  confirmFavorites = () => {
+    this.setState({
+      firstVisit: false,
+      page: 'dashboard'
+    });
+    localStorage.setItem('dazhCrypto', JSON.stringify({
+      test: 'hello',
+    }))
+  }
+
   setPage = page => this.setState({page})
+
+  savedSettings() {
+    let dashboardData = JSON.parse(localStorage.getItem('dazhCrypto'));
+    if(!dashboardData) {
+      return { page: 'settings', firstVisit: true };
+    }
+    return {};
+  }
 
   render() {
     return (
